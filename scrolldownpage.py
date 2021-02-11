9655f55cb55ead87ec67a5ba94aa546aa42a1631
vikky2 <vikky.ranjan2@gmail.com>
2021-02-12 00:54:50 +0530

    another way fro scrolling for the particular falg

--- scrolldownpage.py
+++ scrolldownpage.py
@@ -6,4 +6,7 @@ driver.get("https://www.countries-ofthe-world.com/flags-of-the-world.html")
 
 driver.maximize_window()
 #scroll down page pixel
-driver.execute_script("window.scrollBy(0, 1000)", "")
+#driver.execute_script("window.scrollBy(0, 1000)", ""
+
+flag=driver.find_element_by_xpath("//td[contains(text(),'India')]")
+driver.execute_script("arguments[0].scrollIntoView();",flag)
